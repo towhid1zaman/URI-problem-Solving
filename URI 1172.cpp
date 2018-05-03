86d318237638df35d194abc4e1517b6b0794c815
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2018-05-03 11:05:26 +0600

    URI 1172.cpp

---
+++ URI 1172.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X[10];
+    for(int i=0;i<10;i++)
+    {
+        cin>>X[i];
+
+        if(X[i]<=0)
+        cout<<"X["<<i<<"] = 1"<<endl;
+        else
+            cout<<"X["<<i<<"] = "<<X[i]<<endl;
+
+    }
+    return 0;
+}
